64e56511cca4b1620fa013c1829a4b892caa66ec
BrentWar <142748469+BrentWar@users.noreply.github.com>
2023-09-15 00:13:54 -0400

    Implement different color patterns

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-//add a row of 8 then another then another till 8 rows
+
 let size = 8;
+let choice = "Normal";
 let container = document.querySelector(".container");
 let column = [];
 let grid = new Array(size);
@@ -24,10 +25,55 @@ function resize() {
 
             grid[i][j] = document.createElement("div");
             grid[i][j].classList.add("square");
+
+
+            grid[i][j].addEventListener("mouseenter", function () {
+                let rgb = window.getComputedStyle(grid[i][j]).getPropertyValue("background-color");
+                let sep = rgb.indexOf(",") > -1 ? "," : " ";
+                rgb = rgb.substring(4).split(")")[0].split(sep);
+                
+                let r = "",
+                g = "",
+                b = "";
+                if (+rgb[0] - 29 <= 0) {
+                    r = "0";
+                    g = "0";
+                    b = "0";
+                } else {
+                    r = (+rgb[0]).toString(10) - 29;
+                    g = (+rgb[1]).toString(10) - 29;
+                    b = (+rgb[2]).toString(10) - 29;
+                }
+
+                r = r.toString(16);
+                g = g.toString(16);
+                b = b.toString(16);
+
+                if (r.length == 1)
+                    r = "0" + r;
+                if (g.length == 1)
+                    g = "0" + g;
+                if (b.length == 1)
+                    b = "0" + b;
+
+                grid[i][j].style.backgroundColor = "#" + r + g + b;
+
+                
+            })
+            column[i].appendChild(grid[i][j]);
+
+            /*
+            grid[i][j].addEventListener("mouseenter", function () {
+                grid[i][j].style.background = "#" + Math.floor(Math.random()*16777215).toString(16);
+            })
+            column[i].appendChild(grid[i][j]);
+            /*
+            /*
             grid[i][j].addEventListener("mouseenter", function () {
                 grid[i][j].classList.add("changeColor");
             })
             column[i].appendChild(grid[i][j]);
+            */
         }
     }
 }
@@ -50,4 +96,5 @@ sizeChange.addEventListener("click", function () {
     }
     resize();
 })
+
 
